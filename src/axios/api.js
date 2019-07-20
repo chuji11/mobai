94553701e1c2517c146903e459a51dad8c1bab31
chuji11 <249707696@qq.com>
2019-07-20 21:57:21 +0800

    添加了axios的get和post的简单封装

---
+++ src/axios/api.js
@@ -0,0 +1,10 @@
+// 开发环境
+const DEV_HOST = "http://localhost:8080";
+// 测试环境
+const SET_HOST = "http://129.204.72.71:3000";
+// 生产环境
+const PRO_HOST = "http://129.204.72.71:80";
+
+export default {
+    HOST: DEV_HOST
+};
